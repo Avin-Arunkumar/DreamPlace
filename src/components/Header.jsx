c88fc2bd78251df2f58bafd0e2e775d49ca9e230
Avin-Arunkumar <avinarun2005@gmail.com>
2025-04-11 20:02:53 +0530

    Intial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div
+      className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
+      style={{ backgroundImage: "url('/header_img.png')" }}
+      id="Header"
+    >
+      <Navbar />
+      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+        <h2
+          className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl
+        font-semibold pt-20"
+        >
+          Explore home that fit your dreams
+        </h2>
+        <div className="space-x-12 mt-12">
+          <a
+            href="Projects"
+            className="border border-white px-8 py-3 rounded-3xl hover:bg-blue-400"
+          >
+            Projects
+          </a>
+          <a
+            href="Contact"
+            className="border bg-blue-400 px-8 py-3 rounded-3xl"
+          >
+            Contact us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
