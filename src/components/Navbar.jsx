8c30d76898fb4f50e3ae9755c1c9391160cfa878
Avin-Arunkumar <avinarun2005@gmail.com>
2025-04-12 10:33:19 +0530

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [showMobileMenu]);
   return (
     <div>
       <div className="absolute top-0 left-0 w-full z-10">
@@ -31,7 +38,7 @@ const Navbar = () => {
             }}
             src={assets.menu_icon}
             className="md:hidden w-7 pr-2"
-            alt=""
+            alt="menu"
           />
           {/*  ------mobil-menu-------*/}
           <div
@@ -50,19 +57,37 @@ const Navbar = () => {
               />
             </div>
             <ul className="flex flex-col items-center gap-2 mt-5,px-5,text-lg font-medium">
-              <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+              <a
+                onClick={() => {
+                  setShowMobileMenu(false);
+                }}
+                href="#Header"
+                className="px-4 py-2 rounded-full inline-block"
+              >
                 Home
               </a>
-              <a href="#About" className="px-4 py-2 rounded-full inline-block">
+              <a
+                onClick={() => {
+                  setShowMobileMenu(false);
+                }}
+                href="#About"
+                className="px-4 py-2 rounded-full inline-block"
+              >
                 About
               </a>
               <a
+                onClick={() => {
+                  setShowMobileMenu(false);
+                }}
                 href="#Project"
                 className="px-4 py-2 rounded-full inline-block"
               >
                 Projects
               </a>
               <a
+                onClick={() => {
+                  setShowMobileMenu(false);
+                }}
                 href="#Testimonials"
                 className="px-4 py-2 rounded-full inline-block"
               >
