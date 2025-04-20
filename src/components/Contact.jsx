b52722d9e1e6ec7944d5e83fb2b36072b4f6d27b
Avin-Arunkumar <avinarun2005@gmail.com>
2025-04-20 15:47:57 +0530

    Final Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,6 +12,42 @@ const Contact = () => {
       <p className="  text-center text-gray-600 max-w-80 center mx-auto mb-12">
         Ready to Make a Move? Let’s Build Your Future Together
       </p>
+      <form className="max-w-2xl mx-auto text-gray-600 pt-8">
+        <div className="flex gap-5  justify-center ">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              name="Name"
+              placeholder="Your name"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left">
+            E-mail
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="email"
+              name="E-mail"
+              placeholder="E-mail"
+              required
+            />
+          </div>
+        </div>
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            className=" w-full border border-gray-300 rounded py-3 px-3 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <button className="border px-5 py-2 rounded-2xl text-white bg-blue-500 hover:bg-green-500">
+          Send Message
+        </button>
+      </form>
     </div>
   );
 };
